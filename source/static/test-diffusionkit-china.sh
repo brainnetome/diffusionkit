5eb28cdbbb3e9d9770d805d96ca3248ef5c8394b
Liangfu Chen <liangfu.chen@nlpr.ia.ac.cn>
2016-03-10 20:08:14 +0800

    test diffusionkit for china;

---
+++ source/static/test-diffusionkit-china.sh
@@ -0,0 +1,23 @@
+# install the program  
+wget https://github.com/liangfu/diffusionkit/releases/download/v1.2-r160310/DiffusionKitSetup-x86_64-v1.2-r160310.tar.gz
+tar zxvf DiffusionKitSetup-x86_64-v1.2-r160310.tar.gz
+export PATH=$PATH:`pwd`/DiffusionKitSetup-x86_64-v1.2-r160310/bin
+
+# get the data and run!
+if [ ! -f list.txt ]; then
+  wget https://github.com/liangfu/diffusionkit/releases/download/v1.2-r160310/list.txt
+fi
+if [ ! -f sub01.tar.gz ]; then
+  wget http://ddl.escience.cn/ff/emBl?func=download -O sub01.tar.gz
+fi
+if [ ! -f sub02.tar.gz ]; then
+  wget http://ddl.escience.cn/ff/emBm?func=download -O sub02.tar.gz
+fi
+if [ ! -f atlas.tar.gz ]; then
+  wget http://ddl.escience.cn/ff/emBo?func=download -O atlas.tar.gz
+fi
+if [ ! -f process_advanced.sh ]; then
+  wget https://raw.githubusercontent.com/liangfu/diffusionkit/master/source/static/process_advanced.sh
+fi
+chmod +x process_advanced.sh
+./process_advanced.sh	
