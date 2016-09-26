98cd8a38d14270c1200fc6c05e59ef1ff7915410
Sangma <sangmaxie@gmail.com>
2016-09-26 13:08:47 +0800

    update the download link in test-diffusionkit.sh

--- source/static/test-diffusionkit.sh
+++ source/static/test-diffusionkit.sh
@@ -1,20 +1,20 @@
 # install DiffusionKit
-wget https://github.com/brainnetome/diffusionkit/releases/download/v1.3-r160618/DiffusionKitSetup-x86_64-v1.3-r160618.tar.gz
-tar zxvf DiffusionKitSetup-x86_64-v1.3-r160618.tar.gz
-export PATH=$PATH:$(pwd)/DiffusionKitSetup-x86_64-v1.3-r160618/bin
+wget https://github.com/brainnetome/diffusionkit/releases/download/v1.3-r160618/DiffusionKitSetup-x86_64-v1.3-r160923.tar.gz
+tar zxvf DiffusionKitSetup-x86_64-v1.3-r160923.tar.gz
+export PATH=$PATH:$(pwd)/DiffusionKitSetup-x86_64-v1.3-r160923/bin
 
 # get the data and run!
 if [ ! -f list.txt ]; then
-  wget https://github.com/brainnetome/diffusionkit/releases/download/v1.3-r160618/list.txt
+  wget https://github.com/brainnetome/diffusionkit/releases/download/v1.3-r160923/list.txt
 fi
 if [ ! -f sub01.tar.gz ]; then
-  wget https://github.com/brainnetome/diffusionkit/releases/download/v1.3-r160618/sub01.tar.gz
+  wget https://github.com/brainnetome/diffusionkit/releases/download/v1.3-r160923/sub01.tar.gz
 fi
 if [ ! -f sub02.tar.gz ]; then
-  wget https://github.com/brainnetome/diffusionkit/releases/download/v1.3-r160618/sub02.tar.gz
+  wget https://github.com/brainnetome/diffusionkit/releases/download/v1.3-r160923/sub02.tar.gz
 fi
 if [ ! -f atlas.tar.gz ]; then
-  wget https://github.com/brainnetome/diffusionkit/releases/download/v1.3-r160618/atlas.tar.gz
+  wget https://github.com/brainnetome/diffusionkit/releases/download/v1.3-r160923/atlas.tar.gz
 fi
 if [ ! -f process_advanced.sh ]; then
   wget https://raw.githubusercontent.com/brainnetome/diffusionkit/master/source/static/process_advanced.sh
